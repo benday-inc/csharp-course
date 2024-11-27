8f60d18470875555ad10cbf31395a5f9577ad740
Benjamin Day <benday@benday.com>
2024-11-27 11:19:36 -0500

    Added after version for lab01

---
+++ code/lab-01/after/DebuggingLab/DebuggingLab/ProcessedData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace DebuggingLab;
+
+public class ProcessedData
+{
+    public string Username { get; set; } = string.Empty;
+    public List<string> Data { get; set; } = new();
+}
