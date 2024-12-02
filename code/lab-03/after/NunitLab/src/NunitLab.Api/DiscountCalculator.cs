7d36fd44a96aa24729ef2b36e30fa50b60d83917
Benjamin Day <benday@benday.com>
2024-12-02 17:24:37 -0500

    Implemented lab 03-04

---
+++ code/lab-03/after/NunitLab/src/NunitLab.Api/DiscountCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace NunitLab.Api;
+
+public class DiscountCalculator
+{
+    public decimal CalculateDiscount(decimal totalAmount, string membershipType)
+    {
+        return membershipType switch
+        {
+            "Regular" => totalAmount * 0.05m,
+            "Premium" => totalAmount * 0.10m,
+            "VIP" => totalAmount * 0.20m,
+            _ => 0m
+        };
+    }
+}
+
---
+++ code/lab-03/after/NunitLab/test/NunitLab.UnitTests/DiscountCalculatorTests.cs
@@ -0,0 +1,63 @@
+﻿using NunitLab.Api;
+
+namespace NunitLab.UnitTests;
+
+[TestFixture]
+public class DiscountCalculatorTests
+{
+    private DiscountCalculator? _systemUnderTest;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _systemUnderTest = null;
+    }
+
+    public DiscountCalculator SystemUnderTest
+    {
+        get
+        {
+            if (_systemUnderTest == null)
+            {
+                _systemUnderTest = new DiscountCalculator();
+            }
+
+            Assert.That(_systemUnderTest, Is.Not.Null);
+
+            return _systemUnderTest;
+        }
+    }
+
+    [TestCase(100, "Regular", 5)]
+    [TestCase(200, "Premium", 20)]
+    [TestCase(300, "VIP", 60)]
+    [TestCase(150, "Unknown", 0)]
+    public void CalculateDiscount_ShouldReturnExpectedDiscount(
+        decimal totalAmount, string membershipType, decimal expectedDiscount)
+    {
+        // Act
+        decimal actualDiscount = SystemUnderTest.CalculateDiscount(totalAmount, membershipType);
+
+        // Assert
+        Assert.That(actualDiscount, Is.EqualTo(expectedDiscount));
+    }
+
+    public static IEnumerable<object[]> DiscountTestCases()
+    {
+        yield return new object[] { 500m, "Regular", 25m };
+        yield return new object[] { 400m, "Premium", 40m };
+        yield return new object[] { 1000m, "VIP", 200m };
+    }
+
+    [Test, TestCaseSource(nameof(DiscountTestCases))]
+    public void CalculateDiscount_WithTestCaseSource_ShouldReturnExpectedDiscount(
+        decimal totalAmount, string membershipType, decimal expectedDiscount)
+    {
+        // Act
+        decimal actualDiscount = SystemUnderTest.CalculateDiscount(
+            totalAmount, membershipType);
+
+        // Assert
+        Assert.That(actualDiscount, Is.EqualTo(expectedDiscount));
+    }
+}
