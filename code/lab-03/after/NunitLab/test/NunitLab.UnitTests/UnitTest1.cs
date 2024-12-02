21ef93f49a305ec07c8d23f082f9931c6bb9660f
Benjamin Day <benday@benday.com>
2024-12-02 14:52:14 -0500

    adding after version of unit tests

---
+++ code/lab-03/after/NunitLab/test/NunitLab.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace NunitLab.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
